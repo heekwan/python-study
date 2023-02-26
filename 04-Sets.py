eac020863916250eac58e98639b58ed3305efcdf
Heekwan Son <heekwan.son@gmail.com>
2023-02-26 18:40:32 +0900

    8th commit

---
+++ 04-Sets.py
@@ -0,0 +1,14 @@
+# Sets: unordered, mutable, no duplicates
+myset = set()
+
+myset.add(1)
+myset.add(2)
+myset.add(3)
+
+# myset.remove(4)
+# myset.discard(4)
+# myset.clear()
+
+print(myset.pop())
+print(myset.pop())
+print(myset)
