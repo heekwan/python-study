a1c84b2c2cb0d4b8d0d12d50c88b2c5d3aa425ba
Heekwan Son <heekwan.son@gmail.com>
2023-02-26 20:42:44 +0900

    9th commit

--- 05-Strings.py
+++ 05-Strings.py
@@ -1,3 +1,43 @@
 # Strings: ordered, immutable, text representation
-my_string = '''I\'m a programmer.'''
+"""
+from timeit import default_timer as timer
+my_list = ['a'] * 6
+print(my_list)
+
+# bad
+start = timer()
+my_string = ''
+for i in my_list:
+    my_string += i
 print(my_string)
+stop = timer()
+print(stop - start)
+
+# good
+start = timer()
+my_string = ''.join(my_list)
+print(my_string)
+stop = timer()
+print(stop - start)
+"""
+# %
+# var = "Tom"
+# my_string = "the variable is %s" % var
+# print(my_string)
+# 
+# var = 3.123456789
+# my_string = "the variable is %.2f" % var
+# print(my_string)
+
+# .format()
+# var1 = 3.123456789
+# var2 = 6
+# my_string = "the variable is {:.2f} and {}".format(var1, var2)
+# print(my_string)
+
+# f-String
+var1 = 3.123456789
+var2 = 6
+my_string = f"the variable is {var1*2:.2f} and {var2}"
+print(my_string)
+
