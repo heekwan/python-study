cb9c6c155049136a4175d14732fd0bd6ccebfb66
Heekwan Son <heekwan.son@gmail.com>
2023-02-26 12:44:02 +0900

    third commit

--- 01-Lists.py
+++ 01-Lists.py
@@ -1,6 +1,21 @@
 # Lists: ordered, mutable, allows duplicate elements
-mylist  = ["banaan", "cherry", "apple"]
+mylist  = ["banana", "cherry", "apple"]
 print(mylist)
 
-mylist2 = list()
-print(mylist2)
+mylist.append("lemon")
+print(mylist)
+
+mylist.insert(1, "blueberry")
+print(mylist)
+
+item = mylist.pop()
+print(item)
+print(mylist)
+
+item = mylist.remove("cherry")
+print(item)
+print(mylist)
+
+new_list = sorted(mylist)
+print(mylist)
+print(new_list)
